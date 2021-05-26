41971c9c7445f945b13692a4f3b682d65f4ed629
Eric Thomas <edt514@gmail.com>
2021-05-25 23:36:29 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+   
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Eric Thomas">
+	<meta name="description" content="Eric Thomas recreation of the DevChallenges challenges">
+	<title> Title </title>
+	<link rel="shortcut icon" href="img/icon.png">    
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+   a
+</body>
+</html>
